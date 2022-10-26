4e3415325f879e58965dc5913a0c929f394b6f35
Felipe Silva <felipe.silva@boxdelivery.com.br>
2022-10-26 11:39:50 -0300

    Change menu image and varejo button

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 
 function Menu(props) {
+    const [whatsappLink, setWhatsappLink] = useState('http://web.whatsapp.com/send?1=pt_BR&phone=5511998773386');
+    const [whatsappMobileLink, setWhatsappMobileLink] = useState('http://api.whatsapp.com/send?1=pt_BR&phone=5511998773386');
     return (
         <motion.div
             initial={{ opacity: 0, filter: 'blur(8px)' }}
@@ -31,16 +33,16 @@ function Menu(props) {
                         <span className='social-image facebook'></span>
                     </span>
                 </a>
-                <a href="https://www.facebook.com/ManaporAnaBombonatto" target="_blank" rel="noopener noreferrer" className='b2b'>
-                    <p>VAREJO / B2B</p>
-                    <span className='social-image-container'>
-                        <span className='social-image b2b'></span>
-                    </span>
-                </a>
                 <a href="https://anabombonatto.goomer.app/menu" target="_blank" rel="noopener noreferrer" className='goomer'>
                     <p>GOOMER</p>
                     <span className='social-image-container'>
                         <span className='social-image goomer'></span>
+                    </span>
+                </a>
+                <a href={window.screen.width > 1023 ? whatsappLink : whatsappMobileLink} target="_blank" rel="noopener noreferrer" className='b2b'>
+                    <p>11 99877-3386 ana@anabombonatto.com.br</p>
+                    <span className='social-image-container'>
+                        <span className='social-image b2b'></span>
                     </span>
                 </a>
             </div>
